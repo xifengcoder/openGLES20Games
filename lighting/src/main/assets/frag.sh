fa6af2adc509eec60c57f4ac5866b038dd91bf74
xifeng.yang <yxf05427@163.com>
2023-10-03 15:06:29 +0800

    update: 三种光照的合成

--- lighting/src/main/assets/frag.sh
+++ lighting/src/main/assets/frag.sh
@@ -1,6 +1,8 @@
 precision mediump float; //指定浮点相关变量的精度
 uniform float uRadius; //从宿主程序中传入的球半径
 varying vec3 vPosition;//接收从顶点着色器过来的顶点位置
+varying vec4 vAmbient; //接收从顶点着色器过来的环境光分量
+varying vec4 vDiffuse; //接收从顶点着色器过来的散射光分量
 varying vec4 vSpecular; //接收从顶点着色器过来的散射光分量
 
 void main()
@@ -21,5 +23,5 @@ void main()
    //最终颜色
    vec4 finalColor = vec4(color,0);
    //给此片元颜色值
-   gl_FragColor = finalColor * vSpecular;
+   gl_FragColor = finalColor * vAmbient + finalColor * vDiffuse + finalColor * vSpecular;
 }
--- lighting/src/main/assets/vertex.sh
+++ lighting/src/main/assets/vertex.sh
@@ -7,34 +7,42 @@ attribute vec3 aPosition;  //顶点位置
 attribute vec3 aNormal;  //顶点法向量
 
 varying vec3 vPosition;//用于传递给片元着色器的顶点位置
+varying vec4 vAmbient; //用于传递给片元着色器的环境光最终强度
+varying vec4 vDiffuse; //用于传递给片元着色器的散射光最终强度
 varying vec4 vSpecular; //用于传递给片元着色器的散射光分量
 
 
-void pointLight(                //定位光光照计算的方法
-    in vec3 normal,            //法向量
-    inout vec4 specular,        //镜面反射光分量
-    in vec3 lightLocation,        //光源位置
-    in vec4 lightSpecular        //镜面光强度
+void pointLight(                      //定位光光照计算的方法
+    	inout vec4 lightAmbient,			//环境光最终强度
+        inout vec4 lightDiffuse,		//散射光最终强度
+        inout vec4 lightSpecular,			//镜面光最终强度
+        in vec3 lightLocation,			  //光源位置
+        in vec4 ambientFactor,			  //环境光强度
+        in vec4 diffuseFactor,			  //散射光强度
+        in vec4 specularFactor			  //镜面光强度
 ){
-    vec3 normalTarget = aPosition + normal;    //计算变换后的法向量
-    vec4 fragPos = uMMatrix * vec4(aPosition,1);
-    vec3 modifiedNormal = (uMMatrix * vec4(normalTarget,1)).xyz - fragPos.xyz;
-    modifiedNormal = normalize(modifiedNormal);    //对法向量规格化
-    //计算从表面点到摄像机的向量
-    vec3 viewDir = normalize(uCamera - fragPos.xyz);
+    lightAmbient = ambientFactor;			//直接得出环境光的最终强度
+    vec4 fragPos = uMMatrix * vec4(aPosition, 1);
+    vec3 modifiedNormal = aPosition + aNormal;
+    vec4 normalPos =  uMMatrix * vec4(modifiedNormal, 1);
+    vec3 normalVector = normalize(normalPos.xyz - fragPos.xyz); //法向量
+
+    vec3 eyeDirection= normalize(uCamera - fragPos.xyz);  //计算从表面点到摄像机的向量
     //计算从表面点到光源位置的向量vp
-    vec3 lightDir = normalize(lightLocation - fragPos.xyz);
-    vec3 halfVector = normalize(lightDir + viewDir);    //求视线与光线的半向量
-    float shininess = 50.0;                //粗糙度，越小越光滑
-    float nDotViewHalfVector = dot(modifiedNormal, halfVector);            //法线与半向量的点积
-    float powerFactor = max(0.0, pow(nDotViewHalfVector, shininess));    //镜面反射光强度因子
-    specular = lightSpecular * powerFactor;    //最终的镜面光强度
+    vec3 lightDirection = normalize(lightLocation - fragPos.xyz);
+    vec3 halfVector = normalize(lightDirection + eyeDirection);	//求视线与光线的半向量
+    lightDiffuse = diffuseFactor * max(0.0, dot(normalVector, lightDirection));    //计算散射光的最终强度
+    float specularStrength = max(0.0, pow(dot(normalVector, halfVector), 64.0)); 	//镜面反射光强度因子
+    lightSpecular = specularFactor * specularStrength;    			//计算镜面光的最终强度
 }
 
 void main(){
-    vec4 specular = vec4(0.0,0.0,0.0,0.0);
-    pointLight(normalize(aNormal), specular, uLightLocation, vec4(0.7,0.7,0.7,1.0));//计算镜面光
-    vSpecular = specular;    //将最终镜面光强度传给片元着色器
-    vPosition = aPosition;        //将顶点的位置传给片元着色器
-    gl_Position = uMVPMatrix * vec4(aPosition, 1); //根据总变换矩阵计算此次绘制此顶点的位置
+    vec4 lightAmbient, lightDiffuse, lightSpecular;	  //用来接收三个通道最终强度的变量
+    pointLight(lightAmbient, lightDiffuse, lightSpecular, uLightLocation,
+    		vec4(0.15,0.15,0.15,1.0), vec4(0.8,0.8,0.8,1.0), vec4(0.7,0.7,0.7,1.0));
+	vAmbient = lightAmbient; 		//将环境光最终强度传给片元着色器
+	vDiffuse = lightDiffuse; 		//将散射光最终强度传给片元着色器
+	vSpecular = lightSpecular; 		//将镜面光最终强度传给片元着色器
+	vPosition = aPosition;  //将顶点的位置传给片元着色器
+	gl_Position = uMVPMatrix * vec4(aPosition, 1); //根据总变换矩阵计算此次绘制此顶点的位置
 }
