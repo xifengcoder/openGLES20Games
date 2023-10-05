3d39b6a2dd90c7284a9c9f243a68edea04c8cbe6
xifeng.yang <yxf05427@163.com>
2023-10-05 15:11:57 +0800

    3d obj绘制3: 添加顶点的平均法向量

--- objdrawing/src/main/java/com/bn/objdrawing/LoadUtil.java
+++ objdrawing/src/main/java/com/bn/objdrawing/LoadUtil.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import android.content.res.Resources;
 import android.util.Log;
@@ -27,18 +29,19 @@ public class LoadUtil {
         return new float[]{vector[0] / module, vector[1] / module, vector[2] / module};
     }
 
-    //从obj文件中加载仅携带顶点信息的物体
-    //首先加载顶点信息，再根据顶点组成三角形面的情况自动计算出每个面的法向量
-    //然后将这个面的法向量分配给这个面上的顶点
+    //从obj文件中加载携带顶点信息的物体，并自动计算每个顶点的平均法向量
     public static LoadedObjectVertex loadFromFile(String fname, Resources r, MySurfaceView mv) {
         //加载后3D对象的引用
         LoadedObjectVertex lo = null;
         //原始顶点坐标列表--按顺序从obj文件中加载的
         ArrayList<Float> alv = new ArrayList<Float>();
-        //结果顶点坐标列表 --根据组成面的情况组织好的
+        //顶点组装面索引列表--根据面的信息从文件中加载
+        ArrayList<Integer> alFaceIndex = new ArrayList<Integer>();
+        //结果顶点坐标列表--按面组织好
         ArrayList<Float> alvResult = new ArrayList<Float>();
-        //结果法向量列表--根据组成面的情况组织好的
-        ArrayList<Float> alnResult = new ArrayList<Float>();
+        //平均前各个索引对应的点的法向量集合Map
+        //此HashMap的key为点的索引，value为点所在的各个面的法向量的集合
+        HashMap<Integer, HashSet<Normal>> hmn = new HashMap<>();
 
         try {
             InputStream in = r.getAssets().open(fname);
@@ -46,9 +49,9 @@ public class LoadUtil {
             BufferedReader br = new BufferedReader(isr);
             String temps = null;
 
-            //循环不断从文件中读取行，根据行类型的不同执行
-            //不同的处理逻辑
+            //循环不断从文件中读取行，根据行类型的不同执行不同的处理逻辑
             while ((temps = br.readLine()) != null) {
+                //用空格分割行中的各个组成部分
                 String[] tempsa = temps.split("[ ]+");
                 if (tempsa[0].trim().equals("v")) {//此行为顶点坐标
                     //若为顶点坐标行则提取出此顶点的XYZ坐标添加到原始顶点坐标列表中
@@ -57,37 +60,43 @@ public class LoadUtil {
                     alv.add(Float.parseFloat(tempsa[3]));
                 } else if (tempsa[0].trim().equals("f")) {//此行为三角形面
                     /*
-                     *若为三角形面行则根据 组成面的顶点的索引从原始顶点坐标列表中
-                     *提取相应的顶点坐标值添加到结果顶点坐标列表中，同时根据三个
-                     *顶点的坐标计算出法向量并添加到结果法向量列表中
+                     * 若为三角形面行，则根据组成面的顶点的索引，从alv中提取相应的顶点坐标值，添加到alvResult中，
+                     * 同时根据三个顶点的坐标，计算出此面的法向量，并添加到hmn中。
                      */
 
-                    //提取三角形第一个顶点的坐标
-                    int index = Integer.parseInt(tempsa[1].split("/")[0]) - 1;
-                    float x0 = alv.get(3 * index);
-                    float y0 = alv.get(3 * index + 1);
-                    float z0 = alv.get(3 * index + 2);
+                    int[] index = new int[3];//三个顶点索引值的数组
+
+                    //计算第0个顶点的索引，并获取此顶点的XYZ三个坐标
+                    index[0] = Integer.parseInt(tempsa[1].split("/")[0]) - 1;
+                    float x0 = alv.get(3 * index[0]);
+                    float y0 = alv.get(3 * index[0] + 1);
+                    float z0 = alv.get(3 * index[0] + 2);
                     alvResult.add(x0);
                     alvResult.add(y0);
                     alvResult.add(z0);
 
-                    //提取三角形第二个顶点的坐标
-                    index = Integer.parseInt(tempsa[2].split("/")[0]) - 1;
-                    float x1 = alv.get(3 * index);
-                    float y1 = alv.get(3 * index + 1);
-                    float z1 = alv.get(3 * index + 2);
+                    //计算第1个顶点的索引，并获取此顶点的XYZ三个坐标
+                    index[1] = Integer.parseInt(tempsa[2].split("/")[0]) - 1;
+                    float x1 = alv.get(3 * index[1]);
+                    float y1 = alv.get(3 * index[1] + 1);
+                    float z1 = alv.get(3 * index[1] + 2);
                     alvResult.add(x1);
                     alvResult.add(y1);
                     alvResult.add(z1);
 
-                    //提取三角形第三个顶点的坐标
-                    index = Integer.parseInt(tempsa[3].split("/")[0]) - 1;
-                    float x2 = alv.get(3 * index);
-                    float y2 = alv.get(3 * index + 1);
-                    float z2 = alv.get(3 * index + 2);
+                    //计算第2个顶点的索引，并获取此顶点的XYZ三个坐标
+                    index[2] = Integer.parseInt(tempsa[3].split("/")[0]) - 1;
+                    float x2 = alv.get(3 * index[2]);
+                    float y2 = alv.get(3 * index[2] + 1);
+                    float z2 = alv.get(3 * index[2] + 2);
                     alvResult.add(x2);
                     alvResult.add(y2);
                     alvResult.add(z2);
+
+                    //记录此面的顶点索引
+                    alFaceIndex.add(index[0]);
+                    alFaceIndex.add(index[1]);
+                    alFaceIndex.add(index[2]);
 
                     //通过三角形面两个边向量0-1，0-2求叉积得到此面的法向量
                     //求0号点到1号点的向量
@@ -98,14 +107,21 @@ public class LoadUtil {
                     float vxb = x2 - x0;
                     float vyb = y2 - y0;
                     float vzb = z2 - z0;
-
-                    //通过球两个向量的叉积计算法向量
+                    //通过求两个向量的叉积计算法向量
                     float[] vNormal = vectorNormal(getCrossProduct(vxa, vya, vza, vxb, vyb, vzb));
-                    //将计算出的法向量添加到结果法向量列表中
-                    for (int i = 0; i < 3; i++) {
-                        alnResult.add(vNormal[0]);
-                        alnResult.add(vNormal[1]);
-                        alnResult.add(vNormal[2]);
+                    for (int tempIndex : index) {
+                        //记录每个索引点的法向量到hmn中
+                        //获取当前索引对应点的法向量集合
+                        HashSet<Normal> hsn = hmn.get(tempIndex);
+                        if (hsn == null) {//若集合不存在则创建
+                            hsn = new HashSet<>();
+                        }
+                        //将此点的法向量添加到集合中
+                        //由于Normal类重写了equals方法，因此同样的法向量不会重复出现在此点
+                        //对应的法向量集合中
+                        hsn.add(new Normal(vNormal[0], vNormal[1], vNormal[2]));
+                        //将集合放进HashMap中
+                        hmn.put(tempIndex, hsn);
                     }
                 }
             }
@@ -119,10 +135,17 @@ public class LoadUtil {
             }
 
             //生成法向量数组
-            size = alnResult.size();
-            float[] nXYZ = new float[size];
-            for (int i = 0; i < size; i++) {
-                nXYZ[i] = alnResult.get(i);
+            float[] nXYZ = new float[alFaceIndex.size() * 3];
+            int c = 0;
+            for (Integer i : alFaceIndex) {
+                //根据当前点的索引从Map中取出一个法向量的集合
+                HashSet<Normal> hsn = hmn.get(i);
+                //求出平均法向量
+                float[] tn = Normal.getAverage(hsn);
+                //将计算出的平均法向量存放到法向量数组中
+                nXYZ[c++] = tn[0];
+                nXYZ[c++] = tn[1];
+                nXYZ[c++] = tn[2];
             }
 
             //创建3D对象
